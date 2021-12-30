afb13c74daa32a2c197a473d0c10cf9a49bce295
Diego Almeida <dyhalmeida@gmail.com>
2021-12-30 17:42:59 -0300

    removido css de exemplo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
-  return <h1 className={styles.title}>Hello <span>Next JS</span></h1>
+  return <h1>Hello Next JS</h1>
 }
