cf86cfd0a49bada051782a3521701b45d8d2139f
Diego Almeida <dyhalmeida@gmail.com>
2021-12-31 15:24:50 -0300

    Adicionado next/head específico da página
    Ponto importante:
    
    - O componente `<Head />` importado de `next/head`, pode ser utilizado
    em todas as página para definir o título da página ou metagas expecíficas.
    - Não é recomendável utilizar o `<title>` dentro de _document.tsx, pois
    se não todas as páginas terão os mesmos títulos.
    - O `<Head />` de `next/head` quando utilizado em páginas, todo contéudo
    dele é concatenado ou substítuido no caso de `<title>` dentro do `<Head>`
    de `_document.tsx`.
    - Então recomenda-se que dentro de `_document.tsx` tenha um `<Head>`
    genérico para toda a aplicação e em cada página tenha um `<Head>` específico.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Hello Next JS</h1>
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello Next JS</h1>
+    </>
+  );
 }
