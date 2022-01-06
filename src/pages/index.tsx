f7b41f720017bcc63e5da4819a04c5f5d9b16b4e
Diego Almeida <dyhalmeida@gmail.com>
2022-01-06 08:09:13 -0300

    Adicionado SSG(Static Site Gerenation)
    Pontos importantes:
    
    - O SSR(Server Side Rendering) é executado SEMPRE que uma requisição a página é feita,
    logo, chamadas a API e etc, também é executada junto.
    - SSR é recomendável para páginas com conteúdos dinâmicos, ex: Dashboard com informações dinâmicas de usuários, etc.
    - O SSG(Static Site Gerenation) é executado uma única vez até sua próxima revalidação, ou seja, o conteúdo gerado via SSG é apresentado no browser e "salvo" em um estado estático até que sua revalidação seja expirado. O NextJS exibe esse conteúdo estático já salvo, numa próxima requisição, sem precisar fazer todo o processo de montar o html e fazer requisições para API's novamente.
    Após sua revalidação expirar, o NextJS monta novamente toda a estrutura do HTML + requisições API e etc, "salva" esse estado e apresenta no browser, caso tenha novas informações o conteúdo é atualizado até seu próximo revalidate.
    - SSG é recomendável para páginas estáticas ou quando o conteúdo leva bastante tempo para atualizar, quando o conteúdo é igual para TODOS os usuários, ex: post de blogs, home page, etc.
    - Explicação prática: Se a configuração de revalidação do SSG for de 1 minuto e dentro desses 1 minuto, 100 mil pessoas fizerem requisição para página gerada via SSG, todas essas 100 mil pessoas irão ver o mesmo conteúdo. Depois que a revalidação expirar, o NextJS irá gerar a página estática novamente e se algum contéudo da página for atualizada, as próximas pessoas que fizerem requisições, já terão seus conteúdos atualizados.
    - O Client Side, requisições feitas por components através useEffect é recomendável para conteúdos que não precisam ser indexados ou já estarem prontos no momento que a página é carregada, ex: comentários de blog, etc.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from "../services/stripe";
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps): JSX.Element {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1KEropELrW9ScMWo86B4MaiQ', {
     expand: ['product']
@@ -52,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
